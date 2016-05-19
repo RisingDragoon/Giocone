bfd1847116f61b75e035df6e49f0b50684a78aef
kevin kortho <kevin kortho>
2016-05-19 12:31:28 +0200

    VAR: Il line renderer gira in base alla torretta ma non è preciso con le particelle...

--- Gicoone/Assets/Scripts/Laser.cs
+++ Gicoone/Assets/Scripts/Laser.cs
@@ -7,7 +7,6 @@ public class Laser : MonoBehaviour
 	private Vector3 end;
 	private LineRenderer line;
 	private ParticleSystem part;
-	//private Particle[] particles;
 
 	void Start ()
 	{
@@ -23,10 +22,8 @@ public class Laser : MonoBehaviour
 		bool ok = Physics.Raycast (transform.position, Vector3.forward,out hit, 100);
 		if (ok)
 		{					
-			//Debug.Log (hit.distance);
 			part.startLifetime = hit.distance/5.08f;
 			end = hit.point;
-			//particles = part.GetParticles ();
 		}
 		line.SetPosition (0, start);
 		line.SetPosition (1, end);
